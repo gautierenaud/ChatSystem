596ed9dcf32152bea3277fba81695a700cb33bf7
gautierenaud <gautierenaud@gmail.com>
2015-10-23 08:40:13 +0200

    Moved files from default package to main package

---
+++ src/main/ChatController.java
@@ -0,0 +1,16 @@
+package main;
+
+public class ChatController {
+
+	// singleton pattern for ChatController
+	private static ChatController instance;
+	private ChatController(){}
+
+	public static ChatController INSTANCE() {
+		if (instance == null)
+			instance = new ChatController();
+		return instance;
+	}
+
+
+}
