41bd3f8d409883a005450916c21065265cdd437e
gautierenaud <gautierenaud@gmail.com>
2015-10-28 15:54:30 +0100

    basic GUI behevior tested, we can now open a login window!

--- src/gui/GUIView.java
+++ src/gui/GUIView.java
@@ -4,6 +4,14 @@ package gui;
 
 public class GUIView {
 
+	private ChatGUI controller;
 	
+	public GUIView(ChatGUI chatGUI){
+		controller = chatGUI;
+	}
+	
+	public void OpenLoginWindow(){
+		new Thread(new ViewLogin()).start();
+	}
 	
 }
--- src/gui/ViewElement.java
+++ src/gui/ViewElement.java
@@ -1,43 +1,21 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.*;
+import java.util.*;
 
 /*
  * base abstract Class for all the windows we are going to create for the ChatSystem
  * 
  * e.g. : UserList, ChatWindows, Notification pop-up, ...
  * 
+ * peut être inutile vu qu'on a rien à mettre dedans pour le moment
  */
 
-public class ViewElement extends JFrame {
+public abstract class ViewElement extends JFrame implements Runnable{
 	
 	public ViewElement(){
 		initComponents();
 	}
 	
-	// initialize the view (abstract?)
-	private void initComponents() {
-		/*
-		// create the components
-		// a new label with the "Nom" as value
-		label = new JLabel("Nom: ");
-		// a new text field with 20 columns
-		text = new JTextField(20);
-		// a new button identified as OK
-		button = new JButton("OK");
-		// configures the JFrame layout using a border layout
-		this.setLayout(new BorderLayout());
-		// places the components in the layout
-		this.add("West",label);
-		this.add("Center",text);
-		this.add("East",button);
-		// packs the fenetre: size is calculated
-		// regarding the added components
-		this.pack();
-		// the JFrame is visible now
-		this.setVisible(true);
-		*/
-	}
-
+	abstract void initComponents();
 }
