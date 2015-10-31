320207233fc6861bded7bd9b59f41c073fbc7316
gautierenaud <gautierenaud@gmail.com>
2015-10-31 11:48:41 +0100

    structure for messages

---
+++ src/gui/MessageStruct.java
@@ -0,0 +1,21 @@
+package gui;
+
+public class MessageStruct{
+	
+	private String source;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	private String message;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
