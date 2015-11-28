e17cc4d0aff484ae7274a299e407813f47ca6461
gautierenaud <gautierenaud@gmail.com>
2015-11-28 18:45:57 +0100

    manage the file requests from the outside

---
+++ src/main/ChatFileRequestFromOther.java
@@ -0,0 +1,61 @@
+package main;
+
+import java.util.Stack;
+
+/*
+ * class to handle all the requests from the outside
+ */
+
+
+class FileRequest{
+	private String fileName;
+	private float fileSize;
+	private String sourceID;
+	
+	public FileRequest(String fileName, float fileSize, String sourceID){
+		this.fileName = fileName;
+		this.fileSize = fileSize;
+		this.sourceID = sourceID;
+	}
+	
+	public String getFileName() {
+		return fileName;
+	}
+	
+	public float getFileSize() {
+		return fileSize;
+	}
+
+	public String getSourceID() {
+		return sourceID;
+	}
+}
+
+public class ChatFileRequestFromOther extends Thread{
+	
+	// List of the different requests
+	private Stack<FileRequest> requestStack;
+	
+	public ChatFileRequestFromOther() {
+		requestStack = new Stack<>();
+		this.start();
+	}
+	
+	@Override
+	public void run(){
+		while (true){
+			if (!requestStack.isEmpty()){
+				FileRequest tmp = requestStack.pop();
+				ChatMediator.getInstance().fileRequestQuery(tmp.getFileName(), tmp.getFileSize(), tmp.getSourceID());
+			}
+		}
+	}
+	
+	public void addRequest(String fileName, float fileSize, String sourceID){
+		requestStack.add(new FileRequest(fileName, fileSize, sourceID));
+	}
+	
+	public void clearAll(){
+		requestStack.clear();
+	}
+}
